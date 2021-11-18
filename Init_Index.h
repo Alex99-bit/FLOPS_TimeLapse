453ba5ac9613ac31a56493b09dc32920067e6608
Alex99-bit <alexco_99@hotmail.com>
2021-11-17 23:55:20 -0600

    Prototype 0.0

---
+++ Init_Index.h
@@ -0,0 +1,7 @@
+
+class Init_Index{
+	private:
+		
+	public:
+		
+};
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <windows.h>
+#include <time.h>
+#include "Init_Index.h"
+
+#define sys system
+
+int main(){
+	
+	return 0;
+}
