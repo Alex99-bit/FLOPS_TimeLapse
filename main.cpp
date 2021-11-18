a52703b8ba6bdd12a7e74fec2836d524fa877e1f
Alex99-bit <alexco_99@hotmail.com>
2021-11-18 03:51:02 -0600

    1.3v

--- main.cpp
+++ main.cpp
@@ -9,16 +9,40 @@
 #include <conio.h>
 #include <windows.h>
 #include <time.h>
+#include <cmath>
 #include <pthread.h>
 using namespace std;
+
 #include "Init_Index.h"
 
 #define sys system
 
+typedef struct{
+	Init_Index index;
+	Tempo ttime;
+	Operaciones oper;
+}clases;
+
+void* time(void *arg);
+void* process(void *arg);
+
 // Hilo principal
 int main(){
 	sys("color 0a");
-	Init_Index obj;
-	obj.menu();
+	clases var;
+	
+	pthread_t hilo_time;
+	pthread_t hilo_pros;
+	
+	pthread_create(*hilo_time,NULL,time,&var);
+	pthread_create(*hilo_pros,NULL,time,&var);
 	return 0;
+}
+
+void* time(void *arg){
+	
+}
+
+void* process(void *arg){
+	
 }
